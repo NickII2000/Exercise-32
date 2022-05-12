da1055854a150754c89327bdb46d818e50e453bc
Nick <Nick_II2000@mail.ru>
2022-05-12 19:49:44 +0600

    2-th commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// // console.log(document.body);
-// // console.log(document.head);
-// // console.log(document.documentElement);
-// // console.log(document.body.childNodes);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+console.log(document.body.childNodes);
 // // console.log(document.body.firstChild);
 // console.log(document.body.firstElementChild);
 // // console.log(document.body.lastChild);
@@ -14,9 +14,9 @@
 // console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
 // console.log(document.querySelector('[data-current = "3"]').previousElementSibling);
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
-}
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue;
+//     }
+//     console.log(node);
+// }
