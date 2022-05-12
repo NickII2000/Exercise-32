ad8382913382a57b2dab2c0a67d9133c1a64a7a9
Nick <Nick_II2000@mail.ru>
2022-05-12 19:57:54 +0600

    4-th commit

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 // console.log(document.body.childNodes);
 console.log(document.body.firstChild);
 console.log(document.body.firstElementChild);
-// // console.log(document.body.lastChild);
-// console.log(document.body.lastElementChild);
+console.log(document.body.lastChild);
+console.log(document.body.lastElementChild);
 // // console.log(document.querySelector('#current'));
 // // console.log(document.querySelectorAll('button'));
 // // console.log(document.querySelector('#current').parentNode);
