80d483251b3c097eda699dc83c0d8ef06739a062
Nick <Nick_II2000@mail.ru>
2022-05-12 20:02:32 +0600

    6-th commit

--- script.js
+++ script.js
@@ -6,11 +6,11 @@
 // console.log(document.body.firstElementChild);
 // console.log(document.body.lastChild);
 // console.log(document.body.lastElementChild);
-console.log(document.querySelector('#current'));
-console.log(document.querySelectorAll('button'));
-console.log(document.querySelector('#current').parentNode);
-console.log(document.querySelector('#current').parentElement);
-// // console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current'));
+// console.log(document.querySelectorAll('button'));
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
+console.log(document.querySelector('#current').parentNode.parentNode);
 // console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
 // console.log(document.querySelector('[data-current = "3"]').previousElementSibling);
 
