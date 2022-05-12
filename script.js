78066b1510e49314e28ddac7b32d095096dfa8fd
Nick <Nick_II2000@mail.ru>
2022-05-12 20:06:44 +0600

    8-th commit

--- script.js
+++ script.js
@@ -11,12 +11,12 @@
 // console.log(document.querySelector('#current').parentNode);
 // console.log(document.querySelector('#current').parentElement);
 // console.log(document.querySelector('#current').parentNode.parentNode);
-console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
-console.log(document.querySelector('[data-current = "3"]').previousElementSibling);
+// console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current = "3"]').previousElementSibling);
 
-// for (let node of document.body.childNodes) {
-//     if (node.nodeName == '#text') {
-//         continue;
-//     }
-//     console.log(node);
-// }
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
